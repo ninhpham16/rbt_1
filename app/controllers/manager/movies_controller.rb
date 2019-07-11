bbadf1bc4bd1e65fcea69d7b8bcd12a02a31f150
gdhaison <vuhaisonhb456@gmail.som>
2019-07-11 11:45:05 +0700

    modal update create admin

--- app/controllers/manager/movies_controller.rb
+++ app/controllers/manager/movies_controller.rb
@@ -1,25 +1,59 @@
 module Manager
   class MoviesController < Manager::BaseController
+    skip_before_action :verify_authenticity_token
+    before_action :find_movie, except: [:index, :new, :create]
+    before_action :all_categories, only: [:new, :edit]
+    def index
+      @movies = Movie.all.page(params[:page]).per Settings.per_page_movies
+    end
+
     def new
       @movie = Movie.new
       @categories = Category.all
     end
 
     def create
-      @movie = Movie.new movie_params
-        if @movie.save
+      if @movie.save
         flash[:success] = t ".success"
-        redirect_to new_manager_movie_path
+        redirect_to manager_movies_path
       else 
         flash[:danger] = t ".fails"
         render "new"
       end
     end
 
+    def edit
+      @categories = Category.all
+    end
+
+    def update
+      if @movie.update_attributes movie_params
+        flash[:success] = t ".success"
+        redirect_to manager_movies_path
+      else
+        flash[:danger] = t ".danger"
+        redirect_to manager_movies_path
+      end
+    end
+
+    def destroy
+      Movie.find(params[:id]).destroy
+      flash[:success] = t ".success"
+      redirect_to manager_movies_path
+    end
+
     private
       def movie_params
-        params.require(:movie).permit(:name,:category_id,:director,:actor, 
+        params.require(:movie).permit(:name,:available,:category_id,:director,:actor, 
           :release,:rated,:trailer,:description,:image)
       end
+
+      def find_movie
+        @movie = Movie.find params[:id]
+      end
+      
+      def all_categories
+
+      end  
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ApplicationRecord
   belongs_to :category
 
+  validates :name, presence: true
+  validates :category_id, presence: true 
   has_attached_file :image
   validates_attachment :image,
   content_type: { content_type: /\Aimage\/.*\z/ },
