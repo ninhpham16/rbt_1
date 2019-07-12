d690fe61a877f9613869bd142190ab4ea1fd7a49
ninhpham16 <ninhphamk41sp2@gmail.com>
2019-07-12 14:40:52 +0700

    search-movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,8 @@
 class MoviesController < ApplicationController
+
+  def index
+    @movies = @search.result.page(params[:page]).order(created_at: :desc).per(::Settings.movies)
+  end
   def show
     @movie = Movie.find params[:id]
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,33 @@
 
-admin = Admin.create!(name: "BinhPham",
-                      email: "binh@gmail.com",
-                      password: "123456",
-                      password_confirmation: "123456"
-                     )
+# admin = Admin.create!(name: "BinhPham",
+#                       email: "binh@gmail.com",
+#                       password: "123456",
+#                       password_confirmation: "123456"
+#                      )
 
-user = User.create!(name: "Winfrey",
-             email: "win@gmail.com",
-             full_name: "Oprah Winfrey",
-             phone_number: "0964980884",
-             address: "America",
-             password: "123456",
-             password_confirmation: "123456"
-            )
+# user = User.create!(name: "Winfrey",
+#              email: "win@gmail.com",
+#              full_name: "Oprah Winfrey",
+#              phone_number: "0964980884",
+#              address: "America",
+#              password: "123456",
+#              password_confirmation: "123456"
+#             )
 
-50.times do |n|
-name  = Faker::Name.name
-email = "ex-#{n+1}@gmail.com"
-password = "123456"
-User.create!(name: name,
-            email: email,
-            password: password,
-            full_name: "Oprah Winfrey",
-            phone_number: "0964980884",
-            address: "America",
-            password: "123456",
-            password_confirmation: "123456"
-            )
-end
+# 50.times do |n|
+# name  = Faker::Name.name
+# email = "ex-#{n+1}@gmail.com"
+# password = "123456"
+# User.create!(name: name,
+#             email: email,
+#             password: password,
+#             full_name: "Oprah Winfrey",
+#             phone_number: "0964980884",
+#             address: "America",
+#             password: "123456",
+#             password_confirmation: "123456"
+#             )
+# end
 city = City.create(name: "HaNoi")
 city1 = City.create(name: "HCM")
 
