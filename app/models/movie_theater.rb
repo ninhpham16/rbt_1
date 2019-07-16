6cb98bb1aa21ea067d6e985ca64ef6313fe879e8
pham duong <duongpham91094@gmail.com>
2019-07-16 23:47:01 +0700

    fix support folder

--- app/models/movie_theater.rb
+++ app/models/movie_theater.rb
@@ -1,7 +1,7 @@
 class MovieTheater < ApplicationRecord
   belongs_to :theater
   belongs_to :movie
+  belongs_to :room
 
   scope :date_like, ->(date){where "time LIKE ?", "%#{date}%"}
-  belongs_to :room
 end
