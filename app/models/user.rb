c0b9bc969dbf46eb55fa1a088a1ffca7b52abba9
linhfef2443445 <tomroket@gmail.com>
2019-07-25 09:38:19 +0700

    Restyle Home and Show movie

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :orders
+  has_many :orders, dependent: :destroy
   has_many :reviews, dependent: :destroy
   after_create :welcome_send
   
