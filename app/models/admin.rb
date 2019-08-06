7bf483c61298d50f9e3f40220e8cb6bd94058b0d
ninhpham16 <ninhphamk41sp2@gmail.com>
2019-08-06 15:38:43 +0700

    test-rspec

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,6 +2,8 @@ class Admin < ApplicationRecord
   has_many :notifications, dependent: :destroy
 
   validates :name, presence: true, length: { minimum: 4, maximum: 10 }
+  validates :email, presence: true, length: { minimum: 10, maximum: 20 }
+  validates :password, presence: true, length: { minimum: 6, maximum: 15 }
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
